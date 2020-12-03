d267197442bfc38dc0b8aad99272592299d62c25
Kim jisoo <ishnn032n@gmail.com>
2020-12-04 04:42:55 +0900

    ADD: Temp

--- app/src/main/java/com/ishnn/labtools/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/ishnn/labtools/ui/calculator/CalculatorFragment.kt
@@ -4,28 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.ishnn.labtools.R
+import com.ishnn.labtools.ui.calculator.ui.main.SectionsPagerAdapter
 
 class CalculatorFragment : Fragment() {
-
-    private lateinit var calculatorViewModel: CalculatorViewModel
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        calculatorViewModel =
-                ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_calculator, container, false)
-        val textView: TextView = root.findViewById(R.id.text_calculator)
-        calculatorViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.activity_calculator, container, false)
+        val sectionsPagerAdapter =
+            SectionsPagerAdapter(requireContext(), requireActivity().supportFragmentManager)
+        val viewPager: ViewPager = root.findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = root.findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
         return root
     }
 }
